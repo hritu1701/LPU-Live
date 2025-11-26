ff4576f40e3055e2f58b7beba3dc43c1abead9ae
Hritu Raj <sac3xhin@gmail.com>
2025-11-26 14:13:03 +0530

    Initial Commit

---
+++ LPU Live/LPU_LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LPU_LiveApp.swift
+//  LPU Live
+//
+//  Created by Hritu Raj on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LPU_LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
